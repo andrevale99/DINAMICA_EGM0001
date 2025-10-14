5dce765c2a9f776e85112a7ad51ec083e1ae2c17
andrevale99 <andrefvale@gmail.com>
2025-10-14 16:05:25 -0300

    Código para deixar  a frequência do PWM fixa em 50 Hz

---
+++ code/src/main.c
@@ -0,0 +1,60 @@
+/**
+ * @author Andre Menezes de Freitas Vale
+ *
+ * @brief Codigo que utiliza da interrupcao externas PCINTXX
+ * para criar uma logica de semaforo simples
+ *
+ * @note O PWM é gerado pelo pino PB1 (OC1A) sem o auxilio de interrupcao,
+ * pois a configuracao do TIMER 1 permite isso, ou seja, a mudanca ocorrera
+ * de forma assincrona.
+ *
+ */
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <stdint.h>
+
+#define TOP_COUNTER_VALUE 40000
+
+//===================================================
+//  VARIAVEIS
+//===================================================
+
+//===================================================
+//  PROTOTIPOS
+//===================================================
+
+//===================================================
+//  MAIN
+//===================================================
+int main()
+{
+    DDRB = (1 << DDB1);
+    PORTB = 0x00;
+
+    TCCR1A = (1 << COM1A1) | (1 << COM1A0) | (1 << WGM11);
+    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
+
+    ICR1 = TOP_COUNTER_VALUE;
+
+    OCR1A = 20000;
+
+    sei(); // Ativa as interrupcoes globais
+
+    for (;;)
+    {
+    }
+
+    cli(); // Desativa as interrupcoes globais
+
+    return 0;
+}
+//===================================================
+//  FUNCOES
+//===================================================
